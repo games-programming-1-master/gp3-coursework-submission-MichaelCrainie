54356add91c803ae71f9bbba72beae65f88d7cfe
Crainie <58468077+Crainie@users.noreply.github.com>
2021-01-07 03:55:12 +0000

    update

--- gp3-lab-5-a-Crainie-master/x64/Release/CapsuleShape.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "CapsuleShape.h"
-
-CapsuleShape::CapsuleShape(radius, height, zUP)
-{
-	
-	m_shape = new btCapsuleShape(radius, height, zUP)
-	//m_type = CShapeType::BOX;
-}
